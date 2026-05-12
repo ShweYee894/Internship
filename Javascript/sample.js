28a90061bc8ffe4453dcee46d641656e6a238621
ShweYee894 <shweyeewin462@gmail.com>
2026-05-12 15:54:29 +0630

    add function

--- Javascript/sample.js
+++ Javascript/sample.js
@@ -6,4 +6,9 @@ const clearButton = document.getElementById("clearBtn");
 const deleteButton = document.getElementById("deleteBtn");
 const equalButton = document.getElementById("equalBtn");
 
-
+//call btn functions
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    appendNumber(button.innerText);
+  });
+});
